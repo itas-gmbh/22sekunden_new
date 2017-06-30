791255d80111b6729f8f393c945a15fa31b5b3b7
itas-gmbh <info@itas.cc>
2017-06-30 03:55:59 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 function start() {
-    $.ajax('https://22sekunden.at/wp-content/plugins/22sek-video/record/index.html', {
+    jquery.ajax('https://22sekunden.at/wp-content/plugins/22sek-video/record/index.html', {
         		success: function (data) {
 				jquery('#main').html(data);
 
         		},
         		error: function (data) {
-				hideloader();
             			console.log(data);
 				alert("Verbindunsfehler - try again later");
         		}
